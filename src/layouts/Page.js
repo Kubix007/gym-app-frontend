731191b4e928478d8feb448269d9e4d39e0e8291
Kubix007 <kuba.dolinski@hotmail.com>
2021-04-28 14:26:29 +0200

    LoginPage layout

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import HomePage from '../components/HomePage'
 import NewsPage from '../components/NewsPage';
 import CalendarPage from '../components/CalendarPage';
+import LoginPage from '../components/LoginPage';
 
 
 const Page = () => {
@@ -11,6 +12,7 @@ const Page = () => {
             <Route path="/" exact component={HomePage} />
             <Route path="/aktualnosci" component={NewsPage} />
             <Route path="/kalendarz" component={CalendarPage} />
+            <Route path="/login" component={LoginPage} />
         </>
     );
 }
